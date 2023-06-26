0bcd0615561dfed8c6e195cdf03da53ab4998de5
Ngoc Chung Tran <tranchung163@gmail.com>
2023-06-26 12:16:47 -0500

    Update Catching

--- VR Room/Assets/Scripts/ThrowBall.cs
+++ VR Room/Assets/Scripts/ThrowBall.cs
@@ -15,14 +15,15 @@ public class ThrowBall : MonoBehaviour
     public float throwForce = 10f;
     private Transform rightHand;
     public Rigidbody rb;
-    private Vector3 aVector = new Vector3(0.02f+0.02569938f, 0f, 0f);
+    private Vector3 aVector = new Vector3(0f, 0f, 0.05f);
+    public Vector3 throwDirection = new Vector3(0f, 0f, 0f);
     
 
     void Start()
     {
         
         rightHand = GameObject.Find("mixamorig:RightHand").transform;
-        ballPrefab.transform.position = GameObject.Find("mixamorig:RightHandIndex1").transform.position + aVector;
+        ballPrefab.transform.position = GameObject.Find("mixamorig:RightHandIndex1").transform.position;
         ballPrefab.GetComponent<Transform>().parent = rightHand.parent;
         ballPrefab.GetComponent<Rigidbody>().useGravity = false;
     }
@@ -35,14 +36,14 @@ public class ThrowBall : MonoBehaviour
         ballPrefab.GetComponent<Transform>().parent = null;
         rb = ballPrefab.GetComponent<Rigidbody>();
         rb.useGravity = true;
-        rb.velocity = (ballPrefab.transform.forward * -throwForce);
+        //rb.velocity = (ballPrefab.transform.forward * throwForce);
 
 
         //ballRigidbody.AddForce(throwPosition.forward * throwForce, ForceMode.Impulse);
-        //ballPrefab.GetComponent<Rigidbody>().AddForce(ballPrefab.GetComponent<Transform>().forward * throwForce);
+        //ballPrefab.GetComponent<Rigidbody>().AddForce(ballPrefab.transform.forward * throwForce);
         //GameObject ball = Instantiate(ballPrefab, throwPosition.position, Quaternion.identity);
         //Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
-        //ballRigidbody.AddForce(throwPosition.forward * throwForce, ForceMode.Impulse);
+        ballPrefab.GetComponent<Rigidbody>().AddForce(throwDirection * throwForce, ForceMode.Impulse);
         //GameObject ball = Instantiate(ballPrefab, throwPosition.position, throwPosition.rotation);
         //Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
         //ballRigidbody.AddForce(throwPosition.forward * throwForce, ForceMode.Impulse);
@@ -59,7 +60,7 @@ public class ThrowBall : MonoBehaviour
     void moveBallToFirst()
     {
         rightHand = GameObject.Find("mixamorig:RightHand").transform;
-        ballPrefab.transform.position = GameObject.Find("mixamorig:RightHandIndex1").transform.position;
+        ballPrefab.transform.position = GameObject.Find("mixamorig:RightHandIndex1").transform.position - aVector;
         ballPrefab.GetComponent<Transform>().parent = rightHand.parent;
         ballPrefab.GetComponent<Rigidbody>().useGravity = false;
     }
