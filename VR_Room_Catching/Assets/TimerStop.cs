7a07da56a6f44612f697039c792f85859fec5fc5
Ngoc Chung Tran <tranchung163@gmail.com>
2023-07-10 16:43:02 -0500

    update

--- VR_Room_Catching/Assets/TimerStop.cs
+++ VR_Room_Catching/Assets/TimerStop.cs
@@ -3,32 +3,42 @@ using UnityEngine;
 
 public class TimerStop : MonoBehaviour
 {
+    private const float V = 17.199f;
+    public Animator animator;
     public GameObject walker;
-    bool stop = false;
-    Animator animator;
-    float startTime;
-    float endTime;
-    Vector3 endLine = new Vector3(-0.01621421f, -0.001583072f, 3.810499f); 
+    public float stopDuration = 3f;
+    public float animationStopTime = 10f;
+    public Vector3 firstPosition;
+    private Quaternion firstRotation;
 
-    // Start is called before the first frame update
+
+    private bool isAnimationStopped = false;
+
     private void Start()
     {
+        firstPosition = walker.transform.position;
+        firstRotation = walker.transform.rotation;
         animator = GetComponent<Animator>();
-        startTime = 0f;
-        endTime = 20f;
+        Invoke("StopAnimation", animationStopTime); // Stop animation after animationStopTime seconds
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StopAnimation()
     {
-        startTime += Time.deltaTime;
-        if (startTime >= endTime || walker.transform.position == endLine || GameObject.Find("Room_Modern").transform == walker.transform)
+        if (!isAnimationStopped)
         {
-            Debug.Log("Again");
-            transform.position = new Vector3(0f, 0f, 0f);
-            startTime = 0f;
-       
+            animator.speed = 0f; // Set animation speed to 0 to pause it
+            isAnimationStopped = true;
+            Invoke("ResumeAnimation", stopDuration); // Resume animation after stopDuration seconds
+            walker.transform.position = firstPosition;
+            //walker.transform.rotation = firstRotation;
         }
+    }
 
+    private void ResumeAnimation()
+    {
+        animator.speed = 1f; // Set animation speed to 1 to continue it
+        //animator.Play(0, -1, 0); // Start the animation from the beginning
+        isAnimationStopped = false;
+        Invoke("StopAnimation", animationStopTime); // Stop animation after animationStopTime seconds again
     }
 }
